300b50d3ad4d17bb230b8367be81ebe2d673d2eb
Stanislav B <bazarovstas@gmail.com>
2022-12-05 22:52:43 +0300

    Добавлен js для мобильного меню, также оно работоспособно без js

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,11 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
+import {mobileMenuToogle} from './modules/mobile-menu-toogle';
 
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
-
+  mobileMenuToogle();
   // Utils
   // ---------------------------------
 
---
+++ source/js/modules/mobile-menu-toogle.js
@@ -0,0 +1,34 @@
+function mobileMenuToogle() {
+  let nav = document.querySelector('.main-header');
+  let main = document.querySelector('.main');
+  let toggle = document.querySelector('.main-nav__toogle');
+  let scrollLock = document.querySelector('body');
+  let paddingBlock1 = document.querySelector('.main-header__wrapper');
+  let paddingBlock2 = document.querySelector('.main__wrapper');
+  let paddingBlock3 = document.querySelector('.promo__wrapper');
+
+  nav.classList.remove('main-nav--nojs');
+  nav.classList.add('main-nav--closed');
+  main.classList.remove('main-nav--nojs');
+
+  toggle.addEventListener('click', function () {
+    if (nav.classList.contains('main-nav--closed')) {
+      nav.classList.remove('main-nav--closed');
+      nav.classList.add('main-nav--open');
+      scrollLock.classList.add('scroll-lock');
+      paddingBlock1.classList.add('mobile-menu-open');
+      paddingBlock2.classList.add('mobile-menu-open');
+      paddingBlock3.classList.add('mobile-menu-open');
+
+    } else {
+      nav.classList.add('main-nav--closed');
+      nav.classList.remove('main-nav--open');
+      scrollLock.classList.remove('scroll-lock');
+      paddingBlock1.classList.remove('mobile-menu-open');
+      paddingBlock2.classList.remove('mobile-menu-open');
+      paddingBlock3.classList.remove('mobile-menu-open');
+    }
+  });
+}
+
+export {mobileMenuToogle};
