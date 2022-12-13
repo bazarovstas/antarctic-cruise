ee2e6365ab4295506d94fbe2d095f177adfb30bc
Stanislav B <bazarovstas@gmail.com>
2022-12-14 00:37:42 +0300

    Исправлен js для мобильного меню

---
+++ source/js/modules/init-mobile-menu.js
@@ -0,0 +1,37 @@
+const {body} = document;
+
+const [nav, main, toggle, headerWrapper, mainWrapper, promoWrapper, mainNav]
+  = [
+    '.main-header',
+    '.main-container',
+    '.main-nav__toggle',
+    '.main-header',
+    '.main',
+    '.promo',
+    '.main-nav__list'
+  ].map((cssQuery) => body.querySelector(cssQuery));
+
+function toggleMenu() {
+  nav.classList.toggle('main-nav--closed');
+  nav.classList.toggle('main-nav--open');
+  body.classList.toggle('scroll-lock');
+  headerWrapper.classList.toggle('mobile-menu-open');
+  mainWrapper.classList.toggle('mobile-menu-open');
+  promoWrapper.classList.toggle('mobile-menu-open');
+}
+
+export function initMobileMenu() {
+  if (nav && main && toggle && headerWrapper && mainWrapper && promoWrapper && mainNav) {
+    nav.classList.remove('main-nav--nojs');
+    nav.classList.add('main-nav--closed');
+    main.classList.remove('main-nav--nojs');
+
+    toggle.addEventListener('click', toggleMenu);
+
+    mainNav.addEventListener('click', ({target}) => {
+      if (window.innerWidth <= 767 && target.href) {
+        toggleMenu();
+      }
+    });
+  }
+}
